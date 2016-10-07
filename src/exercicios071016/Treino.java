52d118892afd3a6d124e005c6f186a0227edb013
Userpl022pc09 <Userpl022pc09@SLFRPL022PC09.ensino.atec.local>
2016-10-07 15:37:11 +0100

    1º

---
+++ src/exercicios071016/Treino.java
@@ -0,0 +1,22 @@
+package exercicios071016;
+
+import java.util.Scanner;
+
+public class Treino {
+
+	public Treino() {
+		exercicio1();
+	}
+
+	private void exercicio1() {
+		int num1 = (new Scanner(System.in)).nextInt();
+		int num2 = (new Scanner(System.in)).nextInt();
+		if(num1>num2){
+			System.out.println("O numero "+num1 + " é o maior.");
+		}
+		else{
+			System.out.println("O numero "+num2 + " é o maior.");
+		}
+		
+	}
+}
