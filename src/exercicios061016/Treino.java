fea0a52e7ec12b8351a73d44f83ab8f40493983f
Userpl022pc09 <Userpl022pc09@SLFRPL022PC09.ensino.atec.local>
2016-10-07 14:41:29 +0100

    Primeiro

---
+++ src/exercicios061016/Treino.java
@@ -0,0 +1,23 @@
+package exercicios061016;
+
+public class Treino {
+	public Treino(){
+		System.out.println(exercicio2());
+	}
+	private void exercicio1(){
+		int pontosLeixoes = 45;
+		int pontosFeirense = 76;
+		int pontosPorto = 33;
+		int pontosAves = 67;
+	}
+	
+	private int exercicio2(){
+		int[] pontosFora=new int [] {45, 76, 33, 67};
+		System.out.println(pontosFora[2]);
+		
+		int soma=0;
+		for(int i=0;i<pontosFora.length;i++){
+			soma+=pontosFora[i];}
+		return soma;
+	}
+}
